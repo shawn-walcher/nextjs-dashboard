7c6a97d5dede12232f29ae8c813c453bbe1642f0
Shawn Walcher <55658235+shawn-walcher@users.noreply.github.com>
2026-02-12 11:03:19 -0600

    Setup users page and loading state try to reduce CLS

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,31 @@
 import { Metadata } from "next";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <div>Customers Page</div>;
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query ?? "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -216,3 +216,115 @@ export function InvoicesTableSkeleton() {
     </div>
   );
 }
+
+export function CustomerTableRowSkeleton() {
+  return (
+    <tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+      {/* Customer Name and Image */}
+      <td className="relative overflow-hidden whitespace-nowrap py-3 pl-6 pr-3">
+        <div className="flex items-center gap-3">
+          <div className="h-8 w-8 rounded-full bg-gray-100"></div>
+          <div className="h-6 w-24 rounded bg-gray-100"></div>
+        </div>
+      </td>
+      {/* Email */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-32 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Invoices */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Pending */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Paid */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+    </tr>
+  );
+}
+
+export function CustomersMobileSkeleton() {
+  return (
+    <div className="mb-2 w-full rounded-md bg-white p-4">
+      <div className="flex items-center justify-between border-b pb-4">
+        <div className="flex items-center gap-3">
+          <div className="h-8 w-8 rounded-full bg-gray-100"></div>
+          <div className="h-6 w-24 rounded bg-gray-100"></div>
+        </div>
+      </div>
+      <div className="flex w-full items-center justify-between border-b py-5">
+        <div className="flex w-1/2 flex-col">
+          <div className="h-4 w-12 rounded bg-gray-100 mb-2"></div>
+          <div className="h-6 w-16 rounded bg-gray-100"></div>
+        </div>
+        <div className="flex w-1/2 flex-col">
+          <div className="h-4 w-12 rounded bg-gray-100 mb-2"></div>
+          <div className="h-6 w-16 rounded bg-gray-100"></div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-4 w-32 rounded bg-gray-100"></div>
+      </div>
+    </div>
+  );
+}
+
+export function CustomersTableSkeleton() {
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            <CustomersMobileSkeleton />
+            <CustomersMobileSkeleton />
+            <CustomersMobileSkeleton />
+            <CustomersMobileSkeleton />
+            <CustomersMobileSkeleton />
+            <CustomersMobileSkeleton />
+          </div>
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="rounded-lg text-left text-sm font-normal">
+              <tr>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  Name
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Email
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Total Invoices
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Total Pending
+                </th>
+                <th scope="col" className="px-4 py-5 font-medium">
+                  Total Paid
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              <CustomerTableRowSkeleton />
+              <CustomerTableRowSkeleton />
+              <CustomerTableRowSkeleton />
+              <CustomerTableRowSkeleton />
+              <CustomerTableRowSkeleton />
+              <CustomerTableRowSkeleton />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function SearchSkeleton() {
+  return (
+    <div className={`${shimmer} relative flex flex-1 flex-shrink-0 overflow-hidden rounded-md`}>
+      <div className="h-10 w-full rounded-md bg-gray-100"></div>
+    </div>
+  );
+}
